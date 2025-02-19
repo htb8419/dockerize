2818adf804b71284a49832fdfc8bc2222461bcb1
HT <tayebi1426@gmail.com>
2025-02-19 20:09:05 +0330

    docker

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-wget https://download.docker.com/linux/static/stable/x86_64/docker-27.3.1.tgz -qO- | tar xvfz - docker/docker --strip-components=1
-mv ./docker /usr/local/bin
-mkdir /etc/docker
-cp ./deamon.json /etc/docker/
-cp ./dockerd.service /etc/systemd/system/
+#wget https://download.docker.com/linux/static/stable/x86_64/docker-27.3.1.tgz -qO- | tar xvfz - docker/docker --strip-components=1
+chmod -R +x ./bin/
+cp -drp ./bin/* /usr/local/bin
+if [ ! -d /etc/docker ]; then
+    mkdir /etc/docker;
+fi;
+cp -d ./deamon.json /etc/docker/
+cp -d ./dockerd.service /etc/systemd/system/
 systemctl start dockerd.service
 docker login
 #journalctl -xeu dockerd.service
